26a7798b47df3c4a1b04031faaf2222cd22ddce8
LaraMark <124665841+LaraMark@users.noreply.github.com>
2024-05-22 23:04:11 +0300

    comment

--- ldm_patched/modules/options.py
+++ ldm_patched/modules/options.py
@@ -1,6 +1,10 @@
-
+# Define a global variable 'args_parsing' and initialize it to False
 args_parsing = False
 
 def enable_args_parsing(enable=True):
+    # This function enables or disables argument parsing by modifying the global 'args_parsing' variable
+
+    # Set the global 'args_parsing' variable to the value of the 'enable' parameter
+    # If the 'enable' parameter is not provided, it defaults to True
     global args_parsing
     args_parsing = enable
