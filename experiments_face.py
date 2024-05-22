ce1e526066a53aeedfa5485ae33f1e9df1b0e85e
LaraMark <124665841+LaraMark@users.noreply.github.com>
2024-05-22 23:02:46 +0300

    comment

--- experiments_face.py
+++ experiments_face.py
@@ -1,7 +1,12 @@
+# Import the necessary modules for this script
 import cv2
 import extras.face_crop as cropper
 
-
+# Load the image using OpenCV's `imread` function
 img = cv2.imread('lena.png')
+
+# Crop the image using the `crop_image` function from the `face_crop` module
 result = cropper.crop_image(img)
+
+# Save the cropped image to a new file using OpenCV's `imwrite` function
 cv2.imwrite('lena_result.png', result)
