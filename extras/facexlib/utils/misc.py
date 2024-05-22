913ed529a9fa01f7090659ba055c1845baf5b10e
LaraMark <124665841+LaraMark@users.noreply.github.com>
2024-05-22 23:03:05 +0300

    comment

--- extras/facexlib/utils/misc.py
+++ extras/facexlib/utils/misc.py
@@ -5,39 +5,44 @@ import torch
 from torch.hub import download_url_to_file, get_dir
 from urllib.parse import urlparse
 
+# ROOT_DIR is set to the parent directory of the current file's grandparent directory
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
 def imwrite(img, file_path, params=None, auto_mkdir=True):
-    """Write image to file.
+    """
+    Write image to file.
+
+    This function writes an image array to a specified file path. If the parent folder of the file path does not exist,
+    it can automatically create the folder.
 
     Args:
         img (ndarray): Image array to be written.
         file_path (str): Image file path.
-        params (None or list): Same as opencv's :func:`imwrite` interface.
-        auto_mkdir (bool): If the parent folder of `file_path` does not exist,
-            whether to create it automatically.
+        params (None or list): Additional parameters for OpenCV's imwrite function.
+        auto_mkdir (bool): If set to True, automatically create the parent folder if it doesn't exist.
 
     Returns:
-        bool: Successful or not.
+        bool: Returns True if the image is successfully written, False otherwise.
     """
     if auto_mkdir:
         dir_name = os.path.abspath(os.path.dirname(file_path))
         os.makedirs(dir_name, exist_ok=True)
     return cv2.imwrite(file_path, img, params)
 
-
 def img2tensor(imgs, bgr2rgb=True, float32=True):
-    """Numpy array to tensor.
+    """
+    Numpy array to tensor.
+
+    This function converts a numpy array or a list of numpy arrays to PyTorch tensors. It also handles BGR to RGB conversion
+    and float32 data type conversion.
 
     Args:
-        imgs (list[ndarray] | ndarray): Input images.
-        bgr2rgb (bool): Whether to change bgr to rgb.
-        float32 (bool): Whether to change to float32.
+        imgs (list[ndarray] | ndarray): Input images as a list or a single numpy array.
+        bgr2rgb (bool): If set to True, converts BGR to RGB format.
+        float32 (bool): If set to True, converts the data type to float32.
 
     Returns:
-        list[tensor] | tensor: Tensor images. If returned results only have
-            one element, just return tensor.
+        list[tensor] | tensor: A list or a single tensor, depending on the input.
     """
 
     def _totensor(img, bgr2rgb, float32):
@@ -55,9 +60,22 @@ def img2tensor(imgs, bgr2rgb=True, float32=True):
     else:
         return _totensor(imgs, bgr2rgb, float32)
 
-
 def load_file_from_url(url, model_dir=None, progress=True, file_name=None, save_dir=None):
-    """Ref:https://github.com/1adrianb/face-alignment/blob/master/face_alignment/utils.py
+    """
+    Load a file from a URL and save it to a local directory.
+
+    This function downloads a file from a given URL and saves it to a specified directory. If the directory does not exist,
+    it creates the directory.
+
+    Args:
+        url (str): The URL of the file to download.
+        model_dir (str, optional): The directory where the file will be saved. Defaults to None.
+        progress (bool, optional): If set to True, display the download progress. Defaults to True.
+        file_name (str, optional): The name of the file to save. Defaults to None.
+        save_dir (str, optional): The directory where the file will be saved. Defaults to None.
+
+    Returns:
+        str: The local file path of the downloaded file.
     """
     if model_dir is None:
         hub_dir = get_dir()
@@ -77,19 +95,20 @@ def load_file_from_url(url, model_dir=None, progress=True, file_name=None, save_
         download_url_to_file(url, cached_file, hash_prefix=None, progress=progress)
     return cached_file
 
-
 def scandir(dir_path, suffix=None, recursive=False, full_path=False):
-    """Scan a directory to find the interested files.
+    """
+    Scan a directory and find files with a specific suffix.
+
+    This function scans a directory and returns a generator for file paths that match a specified suffix.
+
     Args:
-        dir_path (str): Path of the directory.
-        suffix (str | tuple(str), optional): File suffix that we are
-            interested in. Default: None.
-        recursive (bool, optional): If set to True, recursively scan the
-            directory. Default: False.
-        full_path (bool, optional): If set to True, include the dir_path.
-            Default: False.
-    Returns:
-        A generator for all the interested files with relative paths.
+        dir_path (str): The directory to scan.
+        suffix (str | tuple(str), optional): The file suffix to search for. Defaults to None.
+        recursive (bool, optional): If set to True, scan subdirectories recursively. Defaults to False.
+        full_path (bool, optional): If set to True, include the full path of the files. Defaults to False.
+
+    Yields:
+        str: The relative path of the file if full_path is False, or the full path if full_path is True.
     """
 
     if (suffix is not None) and not isinstance(suffix, (str, tuple)):
